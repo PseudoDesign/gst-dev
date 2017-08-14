e39a3c9cb49834af80ba58226eaeda1f4e61178b
Adam <ams-tech@users.noreply.github.com>
2017-08-13 23:05:31 -0600

    Able to build gstreamer base, working on modules

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,7 @@ $remote_target = ENV["REMOTE_TARGET"] || "192.168.0.4"
 $remote_user = ENV["REMOTE_USER"] || "root"
 $home = ENV["HOME"] || "/home/vagrant"
 
-target_plugins = ['gstreamer-plugins-bad']
+target_plugins = ['gstreamer-plugins-base', 'gstreamer-plugins-bad']
 
 def get_install_dir(plugin)
   File.join($home, "build", $cc, plugin)
@@ -22,7 +22,7 @@ task :install_base => [:build_base] do
     plugin = "gstreamer"
     install_dir = get_install_dir(plugin)
     sh "mkdir -p /apps/#{plugin}/local"
-    sh "cp -r #{install_dir} /apps/#{plugin}/local"
+    # sh "cp -r #{install_dir} /apps/#{plugin}/local"
   else
     install_remote
   end
@@ -39,7 +39,7 @@ def config_base
     end
     if $cc != cfg
       sh "./autogen.sh"
-      sh "./configure  --prefix=#{install_dir} --enable-gtk-doc --enable-iso-codes --enable-orc"
+      sh "./configure --enable-gtk-doc --enable-iso-codes --enable-orc --with-opengl=yes"
       File.write('.config_target', $cc)
     end
   end
@@ -53,24 +53,51 @@ task :clean_base do
   end
 end
 
+task :clean_modules do
+  target_plugins.each { |plugin|
+    build_dir = get_build_dir(plugin)
+    Dir.chdir(build_dir) do
+      File.delete('.config_target') if File.exist?('.config_target')
+      sh "make clean"
+    end
+  }
+end
+
 task :build_base do
   plugin = "gstreamer"
   install_dir = get_install_dir(plugin)
   build_dir = get_build_dir(plugin)
-  sh "mkdir -p #{install_dir}"
+  # sh "mkdir -p #{install_dir}"
   config_base
   Dir.chdir(build_dir) do
     sh "make -j2 install"
   end
 end
 
+def config_module(plugin)
+  build_dir = get_build_dir(plugin)
+  install_dir = get_install_dir(plugin)
+  Dir.chdir(build_dir) do
+    begin
+      cfg = File.open('.config_target', &:readline)
+    rescue
+      cfg = ""
+    end
+    if $cc != cfg
+      # sh "module load gstreamer/local/gstreamer/"
+      sh "./autogen.sh"
+      sh "./configure  --enable-gtk-doc --enable-iso-codes --enable-orc --with-opengl=yes"
+      File.write('.config_target', $cc)
+    end
+  end
+end
 
 task :install_modules => [:build_modules] do
   if $cc == "gcc"
     target_plugins.each { |plugin|
       install_dir = get_install_dir(plugin)
       sh "mkdir -p /apps/#{plugin}/local"
-      sh "cp -r #{install_dir} /apps/#{plugin}/local"
+    # sh "cp -r #{install_dir} /apps/#{plugin}/local"
     }
   else
     install_remote
@@ -80,11 +107,11 @@ end
 task :build_modules => [:install_base] do
   target_plugins.each { |plugin|
     install_dir = get_install_dir(plugin)
-    autogen = File.join(get_build_dir(plugin), "autogen.sh")
-    configure = File.join(get_build_dir(plugin), "configure")
-    sh "mkdir -p #{install_dir}"
-    sh "module load "
-    sh "#{autogen}"
-    sh "#{configure}  --prefix=#{install_dir} --enable-gtk-doc --enable-iso-codes --enable-orc"
+    build_dir = get_build_dir(plugin)
+  # sh "mkdir -p #{install_dir}"
+    config_module(plugin)
+    Dir.chdir(build_dir) do
+      sh "make -j2 install"
+    end
   }
 end
